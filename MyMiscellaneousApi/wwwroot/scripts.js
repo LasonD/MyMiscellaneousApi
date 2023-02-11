687a990f5bf5ca10c0375acfdd552b34aac9a4a7
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-02-11 21:28:19 +0200

    Add fnaf game

--- MyMiscellaneousApi/wwwroot/scripts.js
+++ MyMiscellaneousApi/wwwroot/scripts.js
@@ -1,10 +1,31 @@
-const btnRedSquare = document.getElementById("btnRedSquare");
-const originalContent = btnRedSquare.innerText;
 
-btnRedSquare.addEventListener("mouseover", (e) => {
-  btnRedSquare.innerText = 'Тисни вже!';
-});
+handleFnafBtn();
+handleRedSquareBtn();
 
-btnRedSquare.addEventListener("mouseout", (e) => {
-  btnRedSquare.innerText = originalContent;
-});
+function handleFnafBtn() {
+  const btnRedSquare = document.getElementById("btnFnaf");
+  const originalContent = btnRedSquare.innerText;
+  
+  btnRedSquare.addEventListener("mouseover", (e) => {
+    btnRedSquare.innerText = '(точно мій)';
+  });
+  
+  btnRedSquare.addEventListener("mouseout", (e) => {
+    btnRedSquare.innerText = originalContent;
+  });
+}
+
+function handleRedSquareBtn() {
+  const btnRedSquare = document.getElementById("btnRedSquare");
+  const originalContent = btnRedSquare.innerText;
+  
+  btnRedSquare.addEventListener("mouseover", (e) => {
+    btnRedSquare.innerText = 'Тисни вже!';
+  });
+  
+  btnRedSquare.addEventListener("mouseout", (e) => {
+    btnRedSquare.innerText = originalContent;
+  });
+}
+
+
